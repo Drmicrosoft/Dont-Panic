6a91ca396a71a4c94746aaaab34ea901161ef0e3
Omar Okasha <omar.prmail@gmail.com>
2016-12-11 10:56:01 +0200

    Create last.cpp

---
+++ last.cpp
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+int main()
+{
+    int nbFloors; // number of floors
+    int width; // width of the area
+    int nbRounds; // maximum number of rounds
+    int exitFloor; // floor on which the exit is found
+    int exitPos; // position of the exit on its floor
+    int nbTotalClones; // number of generated clones
+    int nbAdditionalElevators; // ignore (always zero)
+    int nbElevators; // number of elevators
+    scanf("%d%d%d%d%d%d%d%d", &nbFloors, &width, &nbRounds, &exitFloor, &exitPos, &nbTotalClones, &nbAdditionalElevators, &nbElevators);
+    for (int i = 0; i < nbElevators; i++) {
+        int elevatorFloor; // floor on which this elevator is found
+        int elevatorPos; // position of the elevator on its floor
+        scanf("%d%d", &elevatorFloor, &elevatorPos);
+    }
+int x = 0 ;
+int y=0 ;
+int z = 8 ;
+int k = 0 ;
+    // game loop
+    while (1) {
+        
+        int cloneFloor; // floor of the leading clone
+        int clonePos; // position of the leading clone on its floor
+        char direction[11]; // direction of the leading clone: LEFT or RIGHT
+        scanf("%d%d%s", &cloneFloor, &clonePos, direction);
+
+        // Write an action using printf(). DON'T FORGET THE TRAILING \n
+        // To debug: fprintf(stderr, "Debug messages...\n");
+        if(x%z!=0||y==0)
+        printf("WAIT\n"); // action: WAIT or BLOCK
+        else 
+        {printf("BLOCK\n"); // action: WAIT or BLOCK
+        k++;
+        if(k<=1)
+        z=z+7;
+        else if (k==2)
+        z=5;
+        else
+        z=z+7;
+        x=0;
+        }
+        x++;
+        y++;
+    }
+
+    return 0;
+}
